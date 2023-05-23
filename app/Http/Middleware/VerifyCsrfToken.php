15e7a875d349b20e441de4e3d6661d25c734150e
pandyatama17 <pandyatama17@gmail.com>
2023-05-23 19:10:00 +0700

    23 May init

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        '/ajax/getDesksForAdmin'
+    ];
+    // public function __construct(Application $app, Encrypter $encrypter)
+    // {
+    //     parent::__construct($app, $encrypter);
+    //     $this->except = [
+    //         route('login')
+    //     ];
+    // }
+}
